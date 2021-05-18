6139995490b8a84daf3dee547b31dda425bec75d
ArgNullException <felixbourdages@gmail.com>
2021-05-18 14:30:25 -0400

    Added update that's cool but useless as hell

--- src/main/java/org/github/nullexceptionarg/atplayer/listener/ChatListener.java
+++ src/main/java/org/github/nullexceptionarg/atplayer/listener/ChatListener.java
@@ -4,22 +4,26 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.server.TabCompleteEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.github.nullexceptionarg.atplayer.AtPlayer;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ChatListener implements Listener {
     private AtPlayer instance;
     private FileConfiguration cfg;
+    private static Map<String,Set<UUID>> fakeEvents = new HashMap<>();
 
     public ChatListener(AtPlayer main){
         instance = main;
@@ -27,35 +31,42 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onChatPing(AsyncPlayerChatEvent event) {
-        if (!event.getMessage().contains("@")) return;
-
-        Pattern pingPattern = Pattern.compile("(@([^\\s@]+))");
-        Matcher pingMatcher = pingPattern.matcher(event.getMessage());
-        if (pingMatcher.find()) {
-            StringBuffer buffer = new StringBuffer();
-            List<Player> concernedPlayers = new ArrayList<>();
-            List<Player> colPlayer = new ArrayList<>(Bukkit.getOnlinePlayers());
-            do {
-                String name = pingMatcher.group(2).toUpperCase();
-                cfg = instance.getConfig();
-                for (Player p : colPlayer) {
-
-                    if (p.getDisplayName().toUpperCase().contains(name) || p.getName().toUpperCase().contains(name)) {
-                        pingMatcher.appendReplacement(buffer, ChatColor.AQUA + "@" + p.getName());
-                        if (cfg.getBoolean(p.getUniqueId().toString(),true) && !concernedPlayers.contains(p))
-                            concernedPlayers.add(p);
-                        break;
-                    }
-                }
-            } while (pingMatcher.find());
-            pingMatcher.appendTail(buffer);
-            for (Player p : concernedPlayers) {
-                p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 10, 1);
+        String message = event.getMessage();
+        if(fakeEvents.containsKey(message))
+        {
+            for(UUID p : fakeEvents.get(message)){
+                event.getRecipients().remove(Bukkit.getPlayer(p));
             }
-
-            event.setMessage(buffer.toString());
+            fakeEvents.remove(message);
+            return;
         }
 
+        List<UUID> concernedPlayers = Bukkit.getOnlinePlayers().stream().filter(x -> message.contains(x.getName())).map(Entity::getUniqueId).collect(Collectors.toList());
+        if(concernedPlayers.size() == 0)
+            return;
+
+        cfg = instance.getConfig();
+        List<Player> players = new ArrayList<>(event.getRecipients());
+        for (Player p : players) {
+            if(!concernedPlayers.contains(p.getUniqueId())){
+                event.getRecipients().remove(p);
+            }else{
+                if(cfg.getBoolean(p.getUniqueId().toString(),true))
+                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 10, 1);
+                event.setMessage(event.getMessage().replaceAll(p.getName(), ChatColor.AQUA + "@" + p.getName() + ChatColor.RESET ));
+            }
+        }
+
+        fakeEvents.put(message,new HashSet<>(concernedPlayers));
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(instance, new Runnable() {
+            @Override
+            public void run() {
+                event.getPlayer().chat(message);
+            }
+        },0);
     }
 
 }
+
+
